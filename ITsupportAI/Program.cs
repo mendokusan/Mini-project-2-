0d04ecf5728778ac8d6948ba440279408b05ebe7
Phillip Baxter (1000024452) <BAXTPJ1@student.op.ac.nz>
2018-09-26 13:39:58 +1200

    Basic beginning instructions

--- ITsupportAI/Program.cs
+++ ITsupportAI/Program.cs
@@ -10,6 +10,31 @@ namespace ITsupportAI
     {
         static void Main()
         {
+            string name;
+            Char help;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("Hello what is your name? ");
+            name = Convert.ToString(Console.ReadLine());
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            Console.WriteLine("Hello {0}, what issue are you having: Hardware(H) Software(S) Peripheral(P)?", name);
+            help = Convert.ToChar(Console.ReadLine());
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            if ((help == 'H') || (help == 'h'))
+            {
+                Console.WriteLine("Hardware");
+                Console.WriteLine("Is the issue, ");
+                Console.ReadLine();
+
+
+
+            }
+
         }
     }
 }
