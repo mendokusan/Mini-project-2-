7ef996c87e6d2d71638364677d555ddd0408882e
Phillip Baxter (1000024452) <BAXTPJ1@student.op.ac.nz>
2018-09-26 14:06:14 +1200

    Basic beginning instructions

--- ITsupportAI/Program.cs
+++ ITsupportAI/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ITsupportAI
@@ -10,18 +11,18 @@ namespace ITsupportAI
     {
         static void Main()
         {
-<<<<<<< HEAD
+            //intro
             string name;
             Char help;
 
             Console.ForegroundColor = ConsoleColor.Blue;
 
-            Console.WriteLine("Hello what is your name? ");
+            Console.WriteLine("What is your name? ");
             name = Convert.ToString(Console.ReadLine());
             Thread.Sleep(1000);
             Console.Clear();
 
-            Console.WriteLine("Hello {0}, what issue are you having: Hardware(H) Software(S) Peripheral(P)?", name);
+            Console.WriteLine("Hello {0}, what issue are you having: Hardware(H) Software(S) Peripheral(P) Internet connection (I) None(X)?", name);
             help = Convert.ToChar(Console.ReadLine());
             Thread.Sleep(1000);
             Console.Clear();
@@ -31,14 +32,43 @@ namespace ITsupportAI
                 Console.WriteLine("Hardware");
                 Console.WriteLine("Is the issue, ");
                 Console.ReadLine();
-
-
-
             }
 
-=======
-            Console.WriteLine("HI.");
->>>>>>> 1e02d42771777ca9ad3d228754c439f56d7d8f99
+            if ((help == 'S') || (help == 's'))
+            {
+                Console.WriteLine("Software");
+                Console.WriteLine("Is the issue, ");
+                Console.ReadLine();
+            }
+
+            if ((help == 'P') || (help == 'p'))
+            {
+                Console.WriteLine("Peripheral");
+                Console.WriteLine("Is the issue, ");
+                Console.ReadLine();
+            }
+
+            if ((help == 'I') || (help == 'i'))
+            {
+                Console.WriteLine("Internet connection");
+                Console.WriteLine("Is the issue, ");
+                Console.ReadLine();
+            }
+
+            if ((help == 'X') || (help == 'x'))
+            {
+                Console.WriteLine("No issues today?");
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine(". . . . . ");
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine(" Goodbye ");
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+
+
         }
     }
 }
