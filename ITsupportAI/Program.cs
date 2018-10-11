514ec8098c9474b07a891d13b5a2582585079729
Frydo360 <brandon.martin761122@gmail.com>
2018-10-11 21:08:41 +1300

    Changed some code and did a bunch of the 'Software' section

--- ITsupportAI/Program.cs
+++ ITsupportAI/Program.cs
@@ -12,50 +12,138 @@ namespace ITsupportAI
         static void Main()
         {
             //intro
-            string name;
-            Char help;
+            string name, ans, software;
+            char help;
 
-            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Red;
 
             Console.WriteLine("What is your name? ");
             name = Convert.ToString(Console.ReadLine());
             Thread.Sleep(1000);
             Console.Clear();
 
-            Console.WriteLine("Hello {0}, what issue are you having: Hardware(H) Software(S) Peripheral(P) Internet connection (I) None(X)?", name);
-            help = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Hello {0}, what kind of issues are you having: \nHardware(H) \nSoftware(S) \nPeripheral(P) \nInternet connection (I) \nNone(X)", name);
+            help = Convert.ToChar(Console.ReadLine().ToUpper());
             Thread.Sleep(1000);
             Console.Clear();
 
-            if ((help == 'H') || (help == 'h'))
+            if (help == 'H')
             {
                 Console.WriteLine("Hardware");
                 Console.WriteLine("Is the issue, ");
                 Console.ReadLine();
             }
 
-            if ((help == 'S') || (help == 's'))
+
+            if (help == 'S')
             {
                 Console.WriteLine("Software");
-                Console.WriteLine("Is the issue, ");
+                Console.WriteLine("Is the issue happening to all software? Y/N");
+
+                ans = Convert.ToString(Console.ReadLine().ToUpper());
+                if (ans == "Y")
+                {
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    Console.WriteLine("Have you tried updating the system software? Y/N");
+                    ans = Convert.ToString(Console.ReadLine().ToUpper());
+
+                    if (ans == "Y")
+                    {
+                        Thread.Sleep(1000);
+                        Console.Clear();
+                        Console.WriteLine("Try re-installing your Operating system and or software");
+                    }
+                    else
+                    {
+                        Thread.Sleep(1000);
+                        Console.Clear();
+                        Console.WriteLine("Update the system software");
+                    }
+                }
+                else if (ans == "N")
+                {
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    Console.WriteLine("What software is it happening to? ");
+                    software = Convert.ToString(Console.ReadLine());
+
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    Console.WriteLine("Have you tried updating {0}? Y/N", software);
+                    ans = Convert.ToString(Console.ReadLine().ToUpper());
+
+                    if (ans == "Y")
+                    {
+                        Thread.Sleep(1000);
+                        Console.Clear();
+                        Console.WriteLine("Have you tried turning your computer off and on again? Y/N");
+                        ans = Convert.ToString(Console.ReadLine().ToUpper());
+
+                        if (ans == "Y")
+                        {
+                            Thread.Sleep(1000);
+                            Console.Clear();
+                            Console.WriteLine("Try re-installing {0}", software);
+                        }
+                        else
+                        {
+                            Thread.Sleep(1000);
+                            Console.Clear();
+                            Console.WriteLine("Turn your computer off and on again");
+                        }
+                    }
+                    else
+                    {
+                        Thread.Sleep(1000);
+                        Console.Clear();
+                        Console.WriteLine("Try to update {0} \nHas that fixed the issue? Y/N", software);
+                        ans = Convert.ToString(Console.ReadLine().ToUpper());
+
+                        if (ans == "Y")
+                        {
+                            Thread.Sleep(1000);
+                            Console.Clear();
+                            Console.WriteLine("Thanks for using our IT Support AI.\nGoodbye");
+                            Thread.Sleep(1000);
+                        }
+                        else
+                        {
+                            Thread.Sleep(1000);
+                            Console.Clear();
+                            Console.WriteLine("Try re-installing {0}", software);
+                        }
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    Console.WriteLine("That is not a valid input");
+                    help = Convert.ToChar(Console.ReadLine().ToUpper());
+                }
+
                 Console.ReadLine();
             }
 
-            if ((help == 'P') || (help == 'p'))
+
+            if (help == 'P')
             {
                 Console.WriteLine("Peripheral");
                 Console.WriteLine("Is the issue, ");
                 Console.ReadLine();
             }
 
-            if ((help == 'I') || (help == 'i'))
+
+            if (help == 'I')
             {
                 Console.WriteLine("Internet connection");
                 Console.WriteLine("Is the issue, ");
                 Console.ReadLine();
             }
 
-            if ((help == 'X') || (help == 'x'))
+
+            if (help == 'X')
             {
                 Console.WriteLine("No issues today?");
                 Thread.Sleep(1000);
@@ -67,7 +155,6 @@ namespace ITsupportAI
                 Thread.Sleep(1000);
                 Console.Clear();
             }
-
 
         }
     }
